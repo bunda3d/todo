65391a79b21574d72e92c9450a47c96bb6ea7e8d
bunda3d <bunda3@yahoo.com>
2023-03-05 22:34:02 -0600

    Added task cards to UI

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Task } from 'src/app/interfaces/Task';
+import { TASKS } from 'src/app/interfaces/mock-tasks';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent {
+  tasks: Task[] = TASKS;
+
+
+}
